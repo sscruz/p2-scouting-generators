cbd53b0df865db18b1875fb92d45c6486954edea
Sergio Sanchez Cruz <sergio.sanchez.cruz@cern.ch>
2024-08-02 18:05:27 +0200

    tuning parameters

--- firmware/data.h
+++ firmware/data.h
@@ -1,22 +1,22 @@
 #ifndef ALGO_DATA_H
 #define ALGO_DATA_H
-#define AP_INT_MAX_W 2000
+#define AP_INT_MAX_W 5000
 
 #include "ap_int.h"
 #include "fromcmssw.h" 
 
 
-#define n_pu_jets 30
-#define pu_jet_depth 1
-#define pu_jet_depth2 2
-#define pu_jet_npart 4
+#define n_pu_jets 15
+#define pu_jet_depth 2
+#define pu_jet_depth2 4
+#define pu_jet_npart 8
 
 #define nbits_pt 14
 #define nbits_etaphi 10
 #define nbits_zt 10
 
-#define pu_jet_pt_min 2.0
-#define pu_jet_pt_decay 5.0
+#define pu_jet_pt_min 3.0
+#define pu_jet_pt_decay 12.25
 #define random_bits_per_splitting 19
 #define lut_size 9
 
